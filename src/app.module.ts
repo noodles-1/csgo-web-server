7348bbabb49ab4d10e6a2d9d126bf24768e368ba
noodelz <adrianeroa081202@gmail.com>
2024-07-01 13:01:29 +0800

    connected to mysql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { SettingsModule } from './settings/settings.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { SettingsModule } from '@/settings/settings.module';
+
+import 'dotenv/config'
 
 @Module({
-    imports: [SettingsModule],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: process.env.RDS_ENDPOINT,
+            port: parseInt(process.env.RDS_PORT),
+            username: process.env.RDS_USER,
+            password: process.env.RDS_PASSWORD,
+            database: process.env.RDS_DB,
+            entities: [],
+            synchronize: true,
+        }),
+        SettingsModule
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private dataSource: DataSource) {}
+}
